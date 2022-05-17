22b0cac05901c5757ad2c52f3a49a246979e4507
Angelina Lynch <>
2022-05-17 16:52:29 +0000

    Initial commit

---
+++ Labs/Lb18/script.js
@@ -0,0 +1,12 @@
+function color(){
+  document.getElementById("pp").style.background-color="green";
+}
+  function move(){
+  document.getElementById("pp").style.background-color="green";
+}
+function size(){
+  document.getElementById("pp").style.background-color="green";
+}
+function circle(){
+  document.getElementById("pp").style.background-color="green";
+}
